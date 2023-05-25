baa70927933a579ee6fe27628a5b441fb0c8e436
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-24 21:57:22 -0300

    Efetuado Criação da Seeder para popular o banco com os dados fornecidos

--- API_Estacao/database/seeders/DatabaseSeeder.php
+++ API_Estacao/database/seeders/DatabaseSeeder.php
@@ -2,18 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\EstacaoEnergiaSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        $this->call(EstacaoEnergiaSeeder::class);
     }
 }
