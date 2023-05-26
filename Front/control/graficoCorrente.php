bf98d85fbfd0ff13f4b3e480566e4f7ada1b466c
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-26 14:48:46 -0300

    Adicionado tag script para gerar os graficos

---
+++ Front/control/graficoCorrente.php
@@ -0,0 +1,70 @@
+<script>
+var url = 'http://localhost:8000/estacao-energia';
+
+async function getDataFromAPI() {
+    const resposta = await fetch(url).then(function(response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Erro: " + response.status);
+        }
+    });
+    
+    const datas = [];
+    const tensaoFaseRS = [];
+    const tensaoFaseST = [];
+    const tensaoFaseTR = [];
+    const correnteFaseR = [];
+    const correnteFaseS = [];
+    const correnteFaseT= [];
+    
+    for(const elemento in resposta){
+        datas.push(resposta[elemento].Data_Registro.slice(0, 10));
+        correnteFaseR.push(Number(resposta[elemento].Corrente_Fase_R_A));
+        correnteFaseS.push(Number(resposta[elemento].Corrente_Fase_S_A));
+        correnteFaseT.push(Number(resposta[elemento].Corrente_Fase_T_A));
+    }
+
+    const respsostaFinal = [datas, correnteFaseR, correnteFaseS, correnteFaseT]
+
+    return await respsostaFinal;
+}
+
+const elementosCorrente = getDataFromAPI();
+elementosCorrente.then(function(result) {
+    const ctx2 = document.getElementById('myChart2');
+
+    const graficoDois = new Chart(ctx2, {
+        type: 'line',
+        data: {
+        labels: result[0],
+        datasets: [{
+            label: 'Corrente Fase R (A)',
+            data: result[1],
+            borderWidth: 1,     
+        },{
+            label: 'Corrente Fase S (A)',
+            data: result[2],
+            borderWidth: 1
+        },{
+            label: 'Corrente Fase T (A)',
+            data: result[3],
+            borderWidth: 1,
+        }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Comparativo das Medias Diarias das Correntes',
+                }
+            },
+            scales: {
+                y: {
+                beginAtZero: false
+                }
+            }
+        }
+    });
+});
+</script>
---
+++ Front/control/graficoTensao.php
@@ -0,0 +1,72 @@
+<script>
+    async function getDataFromAPI() {
+        var url = 'http://localhost:8000/estacao-energia';
+        
+        const resposta = await fetch(url).then(function(response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Erro: " + response.status);
+            }
+        });
+        
+        const datas = [];
+        const tensaoFaseRS = [];
+        const tensaoFaseST = [];
+        const tensaoFaseTR = [];
+        const correnteFaseR = [];
+        const correnteFaseS = [];
+        const correnteFaseT= [];
+        
+        for(const elemento in resposta){
+            datas.push(resposta[elemento].Data_Registro.slice(0, 10));
+            tensaoFaseRS.push(Number(resposta[elemento].Tensao_Fase_RS_V));
+            tensaoFaseST.push(Number(resposta[elemento].Tensao_Fase_ST_V));
+            tensaoFaseTR.push(Number(resposta[elemento].Tensao_Fase_TR_V));
+        }
+
+        const respsostaFinal = [datas, tensaoFaseRS, tensaoFaseST, tensaoFaseTR]
+
+        return await respsostaFinal;
+    }
+</script>
+
+<script>
+const elementosTensao = getDataFromAPI();
+elementosTensao.then(function(result) {
+    const ctx1 = document.getElementById('myChart1');
+
+    const graficoDois = new Chart(ctx1, {
+        type: 'line',
+        data: {
+        labels: result[0],
+        datasets: [{
+            label: 'Tensão Fase RS (V)',
+            data: result[1],
+            borderWidth: 1,     
+        },{
+            label: 'Tensão Fase ST (V)',
+            data: result[2],
+            borderWidth: 1
+        },{
+            label: 'Tensão Fase TR (V)',
+            data: result[3],
+            borderWidth: 1,
+        }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Comparativo das Medias Diarias das Tensões',
+                }
+            },
+            scales: {
+                y: {
+                beginAtZero: false
+                }
+            }
+        }
+    });
+});
+</script>
