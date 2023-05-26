03ee8fafa7179bb5dc7d04c4c45a4eab8951e745
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-26 12:53:54 -0300

    Alterado logica de filtragem de dados ao criar o banco

--- API_Estacao/database/seeders/EstacaoEnergiaSeeder.php
+++ API_Estacao/database/seeders/EstacaoEnergiaSeeder.php
@@ -14,23 +14,50 @@ class EstacaoEnergiaSeeder extends Seeder
     {   
         $file = fopen("C:\Users\IgorR\OneDrive\Área de Trabalho\Estacao_Energia\ArquivosComOsDados\Estação_Energia.csv", "r");
         $row = 0;
+
+        $dataAnterior = "";
+        $dataAtual = "";
+        $mediaValores = [[],[],[],[],[],[],[],[]];
+
         while ($line = fgetcsv($file, 1000, ",")) {
             if ($row++ == 0) {
                 continue;
             }
             
-            if(explode(";",$line[0])[1] != "NULL"){
-                $estacao_energia = new EstacaoEnergiaModel;
-                $estacao_energia->Data_Registro = new DateTime(explode(";",$line[0])[0]);
-                $estacao_energia->Tensao_Fase_RS_V = floatval(explode(";",$line[0])[1]);
-                $estacao_energia->Tensao_Fase_ST_V = floatval(explode(";",$line[0])[2],);
-                $estacao_energia->Tensao_Fase_TR_V = floatval(explode(";",$line[0])[3]);
-                $estacao_energia->Corrente_Fase_R_A = floatval(explode(";",$line[0])[4]);
-                $estacao_energia->Corrente_Fase_S_A = floatval(explode(";",$line[0])[5]);
-                $estacao_energia->Corrente_Fase_T_A = floatval(explode(";",$line[0])[6]);
-                $estacao_energia->Demanda_kW = floatval(explode(";",$line[0])[7]);
-                $estacao_energia->Potencia_Ativa_kW = floatval(explode(";",$line[0])[8]);
-                $estacao_energia->save();
+            $lineReposnse = explode(";",$line[0]);
+            $dataAtual = substr($lineReposnse[0], 0, 10);
+
+            if($lineReposnse[1] != "NULL"){
+                if($dataAnterior != $dataAtual){
+                    if($dataAnterior == ""){
+                        $dataAnterior = $dataAtual;
+                        continue;
+                    }else{
+                        $estacao_energia = new EstacaoEnergiaModel;
+                        $estacao_energia->Data_Registro = $dataAnterior;
+                        $estacao_energia->Tensao_Fase_RS_V = array_sum($mediaValores[0]) / count($mediaValores[0]);
+                        $estacao_energia->Tensao_Fase_ST_V = array_sum($mediaValores[1]) / count($mediaValores[1]);
+                        $estacao_energia->Tensao_Fase_TR_V = array_sum($mediaValores[2]) / count($mediaValores[2]);
+                        $estacao_energia->Corrente_Fase_R_A = array_sum($mediaValores[3]) / count($mediaValores[3]);
+                        $estacao_energia->Corrente_Fase_S_A = array_sum($mediaValores[4]) / count($mediaValores[4]);
+                        $estacao_energia->Corrente_Fase_T_A = array_sum($mediaValores[5]) / count($mediaValores[5]);
+                        $estacao_energia->Demanda_kW = array_sum($mediaValores[6]) / count($mediaValores[6]);
+                        $estacao_energia->Potencia_Ativa_kW = array_sum($mediaValores[7]) / count($mediaValores[7]);
+                        $estacao_energia->save();
+
+                        $dataAnterior = $dataAtual;
+                        $mediaValores = [[],[],[],[],[],[],[],[]];
+                    }
+                }else{
+                    $mediaValores[0][] = floatval($lineReposnse[1]);
+                    $mediaValores[1][] = floatval($lineReposnse[2]);
+                    $mediaValores[2][] = floatval($lineReposnse[3]);
+                    $mediaValores[3][] = floatval($lineReposnse[4]);
+                    $mediaValores[4][] = floatval($lineReposnse[5]);
+                    $mediaValores[5][] = floatval($lineReposnse[6]);
+                    $mediaValores[6][] = floatval($lineReposnse[7]);
+                    $mediaValores[7][] = floatval($lineReposnse[8]);
+                }
 
             }else{
                 continue;
