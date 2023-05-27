5c0623a2f4af7b702d5fa7722495b65e3b19dc3c
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-27 12:53:16 -0300

    criado a rota para Potencia ativa

--- API_Estacao/routes/web.php
+++ API_Estacao/routes/web.php
@@ -8,6 +8,7 @@ $router->get('/', function () use ($router) {
 
 $router->get("/estacao-energia/{id}", "EstacaoEnergiaController@consultaPorId");
 $router->get("/estacao-energia", "EstacaoEnergiaController@consultarTodos");
+$router->get("/estacao-energia/potencia-ativa", "EstacaoEnergiaController@consultarPotenciaAtiva");
 $router->post("/estacao-energia/inserir", "EstacaoEnergiaController@inserir");
 $router->delete("/estacao-energia/{id}", "EstacaoEnergiaController@deletar");
 $router->patch("/estacao-energia/{id}", "EstacaoEnergiaController@alterar");
