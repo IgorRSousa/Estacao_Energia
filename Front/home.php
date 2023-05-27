61cefa61b181c8caf5134d6e9f993a7ac3d6a0ed
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-27 17:51:08 -0300

    Efetuado a criação de uma apresentação na tela Home

--- Front/home.php
+++ Front/home.php
@@ -1,1 +1,9 @@
+<div id="TituloInicial">
+    <h1 class="title">Bem vindo, esse é o Projeto da Estação de Energia</h1>
 
+    <p id="textoApresentacao">A ideia desse projeto tem como principal objetivo fazer um compartivo estatistico de alguns dados fonercidos nece caso as medições feitas em uma Estação de Energia esses dados são fornecidos a partir de um arquivo .csv é a partir dele foi feito a filtragem e mineração para exibição do resultado que será visto.</p>
+    
+    <br>
+    <p>Enfim, para ver os resultados basta usar o menu superior.</p>
+
+</div>
