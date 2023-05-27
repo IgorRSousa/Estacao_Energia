6a0b276692b43f2a8031d17d78ee0b3ac327aac8
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-27 11:43:45 -0300

    Criação da rota para a tela de Comparativo e criação da tela de comparatico com os graficos removidos do home

---
+++ Front/comparativoTensaoCorrente.php
@@ -0,0 +1,5 @@
+<canvas id="myChart1"></canvas>
+<?php include("C:\Users\IgorR\OneDrive\Área de Trabalho\Estacao_Energia\Front\control\graficoTensao.php"); ?>
+
+<canvas id="myChart2"></canvas>
+<?php include("C:\Users\IgorR\OneDrive\Área de Trabalho\Estacao_Energia\Front\control\graficoCorrente.php"); ?>
--- index.php
+++ index.php
@@ -14,30 +14,61 @@
     <title>Resultado Mineração</title>
 </head>
 <body>
-<canvas id="myChart1"></canvas>
-<canvas id="myChart2"></canvas>
 
-<?php
-    $request = $_SERVER['REQUEST_URI'];
+    <nav id="navBarId" class="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+            </a>
 
-    switch ($request) {
-        case '/' :
-            require __DIR__ . '/Front/home.php';
-            break;
-        case '' :
-            require __DIR__ . '/Front/home.php';
-            break;
-        case '/index.php' :
-            require __DIR__ . '/Front/home.php';
-            break;
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
 
-        
-        
-        default:
-            http_response_code(404);
-            require __DIR__ . '/views/404.php';
-            break;
-    }
-?>
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="/">
+                    Home
+                </a>
+
+                <a class="navbar-item" href="/Comparativo">
+                    Comparativo Tensao e Corrente
+                </a>
+            </div>
+        </div>
+    </nav>
+
+    <div id="corpo" class="box">
+        <?php
+            $request = $_SERVER['REQUEST_URI'];
+
+
+            switch ($request) {
+                case '/' :
+                    require __DIR__ . '/Front/home.php';
+                    break;
+                case '' :
+                    require __DIR__ . '/Front/home.php';
+                    break;
+                case '/index.php' :
+                    require __DIR__ . '/Front/home.php';
+                    break;
+                
+                case '/Comparativo':
+                    require __DIR__. '/Front/comparativoTensaoCorrente.php';
+                    break;
+
+                
+                
+                default:
+                    http_response_code(404);
+                    require __DIR__ . '/Front/404.php';
+                    break;
+            }
+        ?>
+    </div>
 </body>
 </html>
