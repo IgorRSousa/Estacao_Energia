2b7f1592a9079c03a5d5cb4e2581b74005264572
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-27 14:12:27 -0300

    Criado migration para media mensal

---
+++ API_Estacao/database/migrations/2023_05_27_164313_create_estacao_media_mensal_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estacao_media_mensal', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime("Data_Registro");
+            $table->float("Tensao_Fase_RS_V");
+            $table->float("Tensao_Fase_ST_V");
+            $table->float("Tensao_Fase_TR_V");
+            $table->float("Corrente_Fase_R_A");
+            $table->float("Corrente_Fase_S_A");
+            $table->float("Corrente_Fase_T_A");
+            $table->float("Demanda_kW");
+            $table->float("Potencia_Ativa_kW");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estacao_media_mensal');
+    }
+};
