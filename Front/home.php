d3dfc72f426b3acd76d1dd51d388732d0ecf73b7
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-27 11:42:08 -0300

    Removido graficos do home

--- Front/home.php
+++ Front/home.php
@@ -1,8 +1,1 @@
-<div class="columns is-mobile">
-    <div class="column">
-        <?php include("C:\Users\IgorR\OneDrive\Área de Trabalho\Estacao_Energia\Front\control\graficoTensao.php"); ?>
-    </div>
-    <div class="column">
-        <?php include("C:\Users\IgorR\OneDrive\Área de Trabalho\Estacao_Energia\Front\control\graficoCorrente.php"); ?>
-    </div>
-</div>
+
