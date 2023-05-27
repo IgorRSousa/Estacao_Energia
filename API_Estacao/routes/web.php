f1bb05f6744ab2ad39eff21b013fbe5db1e80284
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-27 15:30:10 -0300

    Criado as rotas de EstacaoMediaMensal

--- API_Estacao/routes/web.php
+++ API_Estacao/routes/web.php
@@ -8,7 +8,13 @@ $router->get('/', function () use ($router) {
 
 $router->get("/estacao-energia/{id}", "EstacaoEnergiaController@consultaPorId");
 $router->get("/estacao-energia", "EstacaoEnergiaController@consultarTodos");
-$router->get("/estacao-energia/potencia-ativa", "EstacaoEnergiaController@consultarPotenciaAtiva");
 $router->post("/estacao-energia/inserir", "EstacaoEnergiaController@inserir");
 $router->delete("/estacao-energia/{id}", "EstacaoEnergiaController@deletar");
 $router->patch("/estacao-energia/{id}", "EstacaoEnergiaController@alterar");
+
+$router->get("/estacao-media/{id}", "EstacaoMediaMensalController@consultaPorId");
+$router->get("/estacao-media", "EstacaoMediaMensalController@consultarTodos");
+$router->get("/estacao-media/potencia-ativa", "EstacaoMediaMensalController@consultarPotenciaAtiva");
+$router->post("/estacao-media/inserir", "EstacaoMediaMensalController@inserir");
+$router->delete("/estacao-media/{id}", "EstacaoMediaMensalController@deletar");
+$router->patch("/estacao-media/{id}", "EstacaoMediaMensalController@alterar");
